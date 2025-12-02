158167869bb81522470b64eaf0b30332493924d5
Harsh140710 <sutharharsh108@gmail.com>
2025-12-02 10:57:55 +0530

    still check for clerk authentication on inggest

--- server/dist/config/inggest.js
+++ server/dist/config/inggest.js
@@ -7,20 +7,25 @@ const user_model_1 = require("../models/user.model");
 exports.inggest = new inngest_1.Inngest({ id: "ecommerce-app" });
 const syncUser = exports.inggest.createFunction({ id: "sync-user" }, { event: "clerk/user.created" }, async ({ event }) => {
     await (0, db_1.connectDB)();
-    const { id, email_address, first_name, last_name, image_url } = event.data;
+    const user = event.data;
+    const email = user.email_addresses?.[0]?.email_address || // normal case
+        user.primary_email_address?.email_address || // some Clerk plans
+        null; // fallback
     const newUser = {
-        clerkId: id,
-        email: email_address[0]?.email_address,
-        name: `${first_name || ""} ${last_name || ""}` || "User",
-        imageUrl: image_url,
+        clerkId: user.id,
+        email,
+        name: `${user.first_name || ""} ${user.last_name || ""}`.trim() || "User",
+        imageUrl: user.image_url,
         address: [],
         wishlist: [],
     };
     await user_model_1.User.create(newUser);
+    return { message: "User synced to DB" };
 });
 const deleteUserFromDB = exports.inggest.createFunction({ id: "delete-user-from-db" }, { event: "clerk/user.deleted" }, async ({ event }) => {
     await (0, db_1.connectDB)();
     const { id } = event.data;
     await user_model_1.User.deleteOne({ clerkId: id });
+    return { message: "User deleted from DB" };
 });
 exports.functions = [syncUser, deleteUserFromDB];
