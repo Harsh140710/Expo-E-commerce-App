79b907b231af7cbe063c0b817777c1fc95c85728
Harsh140710 <sutharharsh108@gmail.com>
2025-12-10 19:32:20 +0530

    remove unwanted env paths

--- admin/src/lib/axios.ts
+++ admin/src/lib/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 // export const API_BASE = "https://expo-ecommerce-backend.onrender.com/api";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || import.meta.env.VITE_ADMIN_URL,
+    baseURL: import.meta.env.VITE_API_BASE_URL,
     withCredentials: true,
 });
 
